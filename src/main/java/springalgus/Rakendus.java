ff74e0dd4ea3d25f3cc8b426d35aeae97c9d2aac
Tanel Kuklane <kukltane@greeny.cs.tlu.ee>
2018-03-05 10:45:38 +0200

    korrutustehe

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -16,6 +16,11 @@ public class Rakendus {
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
     }
+	
+	@RequestMapping("/korrutus")
+	int korrutamine(int arv1, int arv2){
+		return arv1*arv2;
+	}
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
