9ba0f457f2bea33868e15532ca42b7d632e73b0f
Jaagup Kippar <jaagup@tlu.ee>
2018-03-05 09:30:38 +0200

    Rakendus

---
+++ src/main/java/springalgus/Rakendus.java
@@ -0,0 +1,24 @@
+package springalgus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class Rakendus {
+	@RequestMapping("/algus")
+    String tervitusfunktsioon() {
+        return "Ahoi!";
+    }
+    
+    @RequestMapping("/tervitus")
+    String tervitus2(String eesnimi){
+        return "Tere, "+eesnimi;
+    }
+ 
+    public static void main(String[] args) {
+		System.getProperties().put("server.port", 40305);
+        SpringApplication.run(Rakendus.class, args);
+    }
+}
